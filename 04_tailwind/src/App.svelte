9e7ceb299adc081ba9c769311cb1d3ee4693357a
Sergei Zheleznov <sksdes@gmail.com>
2021-04-24 15:26:53 +0200

    Add new example 04_tailwind

---
+++ 04_tailwind/src/App.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	export let name: string;
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a class="text-blue-600 underline" href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style style lang="postcss">
+	main {
+		@apply text-center;
+		@apply p-4;
+		max-width: 240px;
+		@apply mx-auto;
+	}
+
+	h1 {
+		@apply text-red-600;
+		@apply uppercase;
+		font-size: 4em;
+		@apply font-thin;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			@apply max-w-none;
+		}
+	}
+</style>
