56fb226592dc0de6a52ad039d6043a8e6d55b06f
Sergei Zheleznov <sksdes@gmail.com>
2021-04-24 16:10:08 +0200

    [04] Update App.svelte

--- 04_tailwind/src/App.svelte
+++ 04_tailwind/src/App.svelte
@@ -1,30 +1,41 @@
 <script lang="ts">
-	export let name: string;
+  export let name: string;
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a class="text-blue-600 underline" href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+<main class="border-indigo-600">
+  <h1>Hello {name}!</h1>
+  <p>
+    Visit the <a
+      class="text-blue-600 text-4xl underline"
+      href="https://svelte.dev/tutorial">Svelte tutorial</a
+    > to learn how to build Svelte apps.
+  </p>
+  <div class="bg-indigo-50">
+    <div
+      class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-24 lg:px-8 lg:flex lg:items-center lg:justify-between"
+    >
+      <h2
+        class="text-3xl font-extrabold tracking-tight text-gray-900 md:text-4xl"
+      >
+        <span class="block">Ready to dive in?</span>
+        <span class="block text-indigo-600">Start your free trial today.</span>
+      </h2>
+      <div class="mt-8 flex lg:mt-0 lg:flex-shrink-0">
+        <div class="inline-flex rounded-md shadow">
+          <a
+            href="/"
+            class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
+          >
+            Get started
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
 </main>
 
-<style style lang="postcss">
-	main {
-		@apply text-center;
-		@apply p-4;
-		max-width: 240px;
-		@apply mx-auto;
-	}
-
-	h1 {
-		@apply text-red-600;
-		@apply uppercase;
-		font-size: 4em;
-		@apply font-thin;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			@apply max-w-none;
-		}
-	}
+<style global>
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
 </style>
