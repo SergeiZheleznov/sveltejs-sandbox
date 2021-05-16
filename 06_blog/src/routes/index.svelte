e69160fda652f0b47fbf09ed175dbf033a2568c1
Sergei Zheleznov <sksdes@gmail.com>
2021-05-16 12:34:47 +0200

    feat(blog): add Tailwind component

--- 06_blog/src/routes/index.svelte
+++ 06_blog/src/routes/index.svelte
@@ -1,2 +1,7 @@
+<script>
+import Tailwind from "$lib/common/Tailwind.svelte";
+
+</script>
+<Tailwind />
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a class="text-blue-600 underline" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
